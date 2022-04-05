2aabfa19a85d9ab27a5ed371541166246c6ebea5
Bintangp02 <80300921+Bintangp02@users.noreply.github.com>
2022-04-05 07:02:51 +0000

    Bntgp02

--- config.js
+++ config.js
@@ -62,18 +62,12 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
 global.packname = 'stickerby'
-global.author = `Ｍａｒｉｎ－ＭＤ
-
-
-
-
-
-ig: senpai_chan_gemoy`
+global.author = `@bntgp02`
 global.wm = 'Ｍａｒｉｎ－ＭＤ'
 global.watermark = wm
 global.botdate = `╭──────────═┅═─────────\n│◪ ${time}\n┊◪ ${week} ${date}\n╰──────────═┅═─────────`
-global.media = 'https://telegra.ph/file/ccb2359d3925fd7af58cd.jpg'
-global.instagram = 'https://instagram.com/senpai_chan_gemoy'
+global.media = 'https://www.wowkeren.com/display/images/photo/2022/01/08/00405016_s8.jpg'
+global.instagram = 'https://instagram.com/bntgp02'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&doScale=true&scaleWidth=500&scaleHeight=500&fontsize=100&fillTextType=0&backgroundColor=%23401620&text='
 global.wait = '_*tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
